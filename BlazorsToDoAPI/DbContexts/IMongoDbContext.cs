937e1dd16834cfc96ddc52e51e9ec5721efc8412
Fernando Martinez <39782229+FernandoAMartinez@users.noreply.github.com>
2022-02-02 01:10:18 -0300

    delete MongoDbContext (obsolete)

--- BlazorsToDoAPI/DbContexts/IMongoDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using MongoDB.Driver;
-
-namespace BlazorsToDoAPI.DbContexts
-{
-    public interface IMongoDbContext<T> where T : class
-    {
-        IMongoCollection<T> GetMongoDbCollection(string collectionName);
-    }
-}
